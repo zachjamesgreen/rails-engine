a4e6a456e59a1b3bad2d779a62eb9770bd02c415
Zach Green <zachjamesgreen@gmail.com>
2021-07-16 23:30:14 -0600

    Move page and per method to parent class

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,16 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.per(per_page)
+    per_page ||= 20
+    offset = @page * per_page
+    return [] if @page != 0 && (self.count / @page) < 1
+    return self.all if offset >= self.count
+    self.limit(per_page).offset(offset)
+  end
+
+  def self.page(page)
+    @page = (page.to_i - 1)
+    return self
+  end
 end
