a9a5879cc6592ffd8bd147159093d0c1dd13aa87
Zach Green <zachjamesgreen@gmail.com>
2021-07-16 23:27:58 -0600

    Implement getting all items for a merchant

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
   def index
-    @merchant = Merchant.find(params[:merchant_id])
-    @items = @merchant.items
-    render json: {
-      count: @items.count,
-      merchant: @merchant,
-      items: @items
-    } 
+    begin
+      items = Merchant.find(params[:merchant_id]).items
+    render json: items
+    rescue ActiveRecord::RecordNotFound
+      render json: {
+        message: "Not Found",
+        errors: ["Could not find Merchant by this id => #{params[:merchant_id]}"]
+      }, status: 404
+    end
   end
 end
--- spec/requests/merchant_items_spec.rb
+++ spec/requests/merchant_items_spec.rb
@@ -9,48 +9,49 @@ RSpec.describe 'Merchants API' do
   it 'returns merchant items in the correct format' do
     id = @merchants[0].id
     get "/api/v1/merchants/#{id}/items"
-    body = JSON.parse(response.body)
+    body = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
     expect(body.has_key?(:data)).to be true
     data = body[:data]
-    expect(data.has_key?(:id)).to be true
-    expect(data.has_key?(:type)).to be true
-    expect(data.has_key?(:attributes)).to be true
-    attrs = data[:attributes]
+    expect(data[0].has_key?(:id)).to be true
+    expect(data[0].has_key?(:type)).to be true
+    expect(data[0].has_key?(:attributes)).to be true
+    attrs = data[0][:attributes]
     expect(attrs.has_key?(:name)).to be true
     expect(attrs.has_key?(:description)).to be true
-    expect(attrs.has_key?(:unit_price)).to be true
-    expect(attrs[:unit_price]).to be_instance_of Float
-    expect(attrs.has_key?(:merchant_id)).to be true
+    expect(attrs.has_key?(:'unit-price')).to be true
+    expect(attrs[:'unit-price']).to be_instance_of Float
+    expect(attrs.has_key?(:'merchant-id')).to be true
   end
 
   it 'returns merchant items' do
     id = @merchants[0].id
     items = @merchants[0].items
     get "/api/v1/merchants/#{id}/items"
-    body = JSON.parse(response.body)
+    body = JSON.parse(response.body, symbolize_names: true)
     data = body[:data]
     expect(response).to be_successful
     expect(data.length).to eq(items.length)
-    expect(data[0][:id]).to eq (items[0].id)
+    expect(data[0][:id].to_i).to eq (items[0].id)
     expect(data[0][:attributes][:name]).to eq (items[0].name)
     expect(data[0][:attributes][:description]).to eq (items[0].description)
-    expect(data[0][:attributes][:unit_price]).to eq (items[0].unit_price)
+    expect(data[0][:attributes][:'unit-price']).to eq (items[0].unit_price)
+    expect(data[0][:attributes][:'merchant-id']).to eq (items[0].merchant_id)
   end
 
   it 'returns an empty array if there are no merchant items' do
     id = @merchants[1].id
     get "/api/v1/merchants/#{id}/items"
-    body = JSON.parse(response.body)
+    body = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
     expect(body[:data]).to eq([])
   end
 
   it 'returns 404 if merchant cant be found' do
     get "/api/v1/merchants/200/items"
-    body = JSON.parse(response.body)
+    body = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_not_found
     expect(body[:message]).to eq 'Not Found'
-    expect(body[:errors]).to match_array ['Could not find Merchant by this id => 0']
+    expect(body[:errors]).to match_array ['Could not find Merchant by this id => 200']
   end
 end
